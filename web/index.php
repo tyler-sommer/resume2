6b8e52a459f01fa1b15f59a1d409f983564ab39e
Tyler Sommer <sommertm@gmail.com>
2014-11-26 17:52:24 -0800

    Added simplistic browser cache control

--- web/index.php
+++ web/index.php
@@ -5,19 +5,54 @@ use Symfony\Component\HttpFoundation\Response;
 use Nice\Application;
 use Nice\Extension\TwigExtension;
 use Nice\Router\RouteCollector;
+use Symfony\Component\HttpKernel\HttpCache\HttpCache;
+use Symfony\Component\HttpKernel\HttpCache\Store;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new Application();
-$app->appendExtension(new TwigExtension(__DIR__ . '/../views'));
+$env = 'prod';
+$app = new Application($env);
 
-$app->set('routes', function (RouteCollector $r) {
-    $r->map('/', 'home', function (Application $app) {
-        return new Response($app->get('twig')->render('index.html.twig'));
+$cacheDir = $app->getCacheDir();
+$templateDir = __DIR__ . '/../views/';
+
+/**
+ * @param Request $request
+ * @param string $template
+ * @return Response
+ */
+$renderCachedPage = function(Request $request, $template) use ($app, $templateDir) {
+    $response = new Response();
+    $response->setLastModified(new \DateTime('@' . filemtime($templateDir.$template)));
+    $response->setPublic();
+    if (($hash = md5_file($templateDir.$template)) !== '') {
+        $response->setEtag($hash);
+    }
+    if ($response->isNotModified($request)) {
+        return $response;
+    }
+
+    $response->setContent($app->get('twig')->render($template));
+
+    return $response;
+};
+
+$app->appendExtension(new TwigExtension($templateDir));
+$app->set('routes', function (RouteCollector $r) use ($renderCachedPage) {
+    $r->map('/', 'home', function (Request $request) use ($renderCachedPage) {
+        return $renderCachedPage($request, 'index.html.twig');
     });
 
-    $r->map('/print', 'print', function (Application $app) {
-        return new Response($app->get('twig')->render('print.html.twig'));
+    $r->map('/print', 'print', function (Request $request) use ($renderCachedPage) {
+        return $renderCachedPage($request, 'print.html.twig');
     });
 });
-$app->run();
+
+$stack = new Stack\Builder();
+$stack->push(function ($app) use ($cacheDir) {
+    return new HttpCache($app, new Store($cacheDir));
+});
+
+$app = $stack->resolve($app);
+
+Stack\run($app);
